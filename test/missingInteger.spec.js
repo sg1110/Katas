9ea893e24a3b740f6bcbb14449d128dcc810e467
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-04 22:55:03 +0000

    another test - missinginteger

--- test/missingInteger.spec.js
+++ test/missingInteger.spec.js
@@ -12,15 +12,7 @@ describe.only("given an array of integers return the smallest positive integer (
     let array = [-1, -3];
     expect(missingInteger(array)).to.deep.equal(1);
   });
+  it("should work for one positive integers", function() {
+    expect(missingInteger([999])).to.deep.equal(1000);
+  });
 });
-
-// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
-// Given A = [1, 2, 3], the function should return 4.
-
-// Given A = [−1, −3], the function should return 1.
-
-// Write an efficient algorithm for the following assumptions:
-
-// N is an integer within the range [1..100,000];
-// each element of array A is an integer within the range [−1,000,000..1,000,000].
