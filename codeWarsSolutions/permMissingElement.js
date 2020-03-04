892d36ace8369a45e934fb7f5aaf75a84afe1dfc
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-04 21:07:56 +0000

    fixed permissing element

--- codeWarsSolutions/permMissingElement.js
+++ codeWarsSolutions/permMissingElement.js
@@ -1,16 +1,37 @@
-module.exports = function element(array) {
-  array = array.sort((a, b) => {
+module.exports = function element(inputArray) {
+  inputArray = inputArray.sort((a, b) => {
     return a - b;
   });
-  let missing = array[0];
-  let index = 0;
-  while (array.length > index) {
-    if (array[index] !== missing + 1) {
-      missing = array[index];
-    }
-    index++;
+  let missing;
+  if (inputArray[0] !== 1) {
+    return 1;
   }
-  return missing - 1;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] + 1 !== inputArray[i + 1]) {
+      missing = inputArray[i] - 1;
+    }
+  }
+  return missing;
 };
-//2, 3, 1, 5
-// 2 3 4 5
+
+// need to fix for nr 1
+// let missing = array[0];
+// let index = 0;
+// while (array.length > index) {
+//   if (array[index] !== missing + 1) {
+//     missing = array[index];
+//   }
+//   index++;
+// }
+// return missing - 1;
+
+// :)
+// inputArray.forEach((number, index) => {
+//   if (number + 1 === inputArray[index + 1] && index === 0) {
+//     return (answer = 1);
+//   }
+//   if (number + 1 === inputArray[index + 1]) {
+//     answer = number - 1;
+//   }
+// });
+// return answer;
