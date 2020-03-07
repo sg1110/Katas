17a861d862cbf02ff827c6b6c6f0006026d94416
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-07 22:31:20 +0000

    countDiv wip

---
+++ codeWarsSolutions/countDiv.js
@@ -0,0 +1,14 @@
+const math = require("mathjs");
+
+module.exports = function cov(start, end, diviser) {
+  const range = math.range(start, end).toArray();
+  let divisibleNumbers = [];
+  range.forEach(number => {
+    if (number % diviser === 0) {
+      divisibleNumbers.push(number);
+    }
+  });
+  return divisibleNumbers.length;
+};
+
+//6-11 / 2 (6 8 10)
