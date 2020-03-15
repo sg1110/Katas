efedd501be4e4caaa3d07e7b1cf5f952426ad726
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-15 15:49:28 +0000

    dictionary replacer using RegExp

--- codeWarsSolutions/dictionaryReplacer.js
+++ codeWarsSolutions/dictionaryReplacer.js
@@ -1,4 +1,4 @@
-module.exports = function dictionaryReplacer(string, dictionary) {
+function dictionaryReplacer(string, dictionary) {
   //optional
   if (!string.length) {
     return string;
@@ -11,25 +11,28 @@ module.exports = function dictionaryReplacer(string, dictionary) {
       }
     }
   }
+
   return string;
+}
+
+function dictionaryReplacer2(string, dictionary) {
+  //optional
+  if (!string.length) {
+    return string;
+  }
+
+  for (const key in dictionary) {
+    string = replaceAll(string, key, dictionary[`${key}`]);
+  }
+
+  function replaceAll(string, key, value) {
+    return string.replace(new RegExp(`\\$${key}\\$`, "g"), value);
+  }
+
+  return string;
+}
+
+module.exports = {
+  dictionaryReplacer,
+  dictionaryReplacer2
 };
-
-//regex g attempt
-// function replaceAll(string, key, value) {
-//     // eslint-disable-next-line no-useless-escape
-//     let regex = "$" + key + "$";
-//     // console log $temp$ (same as variable ugh) but does not work : )))))
-//     //let ugh = "$temp$"
-//     //console.log(regex);
-//     console.log(value)
-//     return string.replace(new RegExp(regex, "g"), value);
-//   }
-
-// aaand another regex attempt
-// for (const key in dictionary) {
-//     // eslint-disable-next-line no-useless-escape
-//     let replace = `\$${key}\$`;
-//     let re = new RegExp(replace, "g");
-//     string = string.replace(re, dictionary[`${key}`]);
-//     // string = string.replace(`$${key}$`, dictionary[`${key}`]);
-//   }
--- test/dictionaryReplacer.spec.js
+++ test/dictionaryReplacer.spec.js
@@ -3,39 +3,91 @@ const dictionaryReplacer = require("../codeWarsSolutions/dictionaryReplacer");
 
 describe("Replace words in a given string based on the corresponding values in dictionary", function() {
   it("should not replace any values when the dictionary is empty", function() {
-    expect(dictionaryReplacer("", {})).to.equal("");
+    expect(dictionaryReplacer.dictionaryReplacer("", {})).to.equal("");
   });
   it("should replace a value in a string when dictionary has only one value", function() {
-    expect(dictionaryReplacer("$temp$", { temp: "temporary" })).to.equal(
-      "temporary"
-    );
+    expect(
+      dictionaryReplacer.dictionaryReplacer("$temp$", { temp: "temporary" })
+    ).to.equal("temporary");
   });
   it("should replace multiple values in a string with dictionary values", function() {
     expect(
-      dictionaryReplacer("$temp$ here comes the name $name$", {
-        temp: "temporary",
-        name: "John Doe"
-      })
+      dictionaryReplacer.dictionaryReplacer(
+        "$temp$ here comes the name $name$",
+        {
+          temp: "temporary",
+          name: "John Doe"
+        }
+      )
     ).to.equal("temporary here comes the name John Doe");
   });
   it("should replace multiple values in a string with dictionary values when some values occur mult. times", function() {
     expect(
-      dictionaryReplacer("$temp$ $temp$ here comes the name $name$", {
-        temp: "temporary",
-        name: "John Doe"
-      })
+      dictionaryReplacer.dictionaryReplacer(
+        "$temp$ $temp$ here comes the name $name$",
+        {
+          temp: "temporary",
+          name: "John Doe"
+        }
+      )
     ).to.equal("temporary temporary here comes the name John Doe");
   });
   it("should not replace prefixed sufixed values in a string that are not found in dictionary", function() {
     expect(
-      dictionaryReplacer("$tempaprampampam$ here comes the name $name$", {
-        temp: "temporary",
-        name: "John Doe"
-      })
+      dictionaryReplacer.dictionaryReplacer(
+        "$tempaprampampam$ here comes the name $name$",
+        {
+          temp: "temporary",
+          name: "John Doe"
+        }
+      )
     ).to.equal("$tempaprampampam$ here comes the name John Doe");
   });
 });
 
+describe.only("Replace words in a given string based on the corresponding values in dictionary", function() {
+  it("should not replace any values when the dictionary is empty", function() {
+    expect(dictionaryReplacer.dictionaryReplacer2("", {})).to.equal("");
+  });
+  it("should replace a value in a string when dictionary has only one value", function() {
+    expect(
+      dictionaryReplacer.dictionaryReplacer2("$temp$", { temp: "temporary" })
+    ).to.equal("temporary");
+  });
+  it("should replace multiple values in a string with dictionary values", function() {
+    expect(
+      dictionaryReplacer.dictionaryReplacer2(
+        "$temp$ here comes the name $name$",
+        {
+          temp: "temporary",
+          name: "John Doe"
+        }
+      )
+    ).to.equal("temporary here comes the name John Doe");
+  });
+  it("should replace multiple values in a string with dictionary values when some values occur mult. times", function() {
+    expect(
+      dictionaryReplacer.dictionaryReplacer2(
+        "$temp$ $temp$ here comes the name $name$",
+        {
+          temp: "temporary",
+          name: "John Doe"
+        }
+      )
+    ).to.equal("temporary temporary here comes the name John Doe");
+  });
+  it("should not replace prefixed sufixed values in a string that are not found in dictionary", function() {
+    expect(
+      dictionaryReplacer.dictionaryReplacer2(
+        "$tempaprampampam$ here comes the name $name$",
+        {
+          temp: "temporary",
+          name: "John Doe"
+        }
+      )
+    ).to.equal("$tempaprampampam$ here comes the name John Doe");
+  });
+});
 // Create a method that takes a string and a dictionary,
 // and replaces every key in the dictionary pre and suffixed with a dollar sign,
 // with the corresponding value from the Dictionary.
