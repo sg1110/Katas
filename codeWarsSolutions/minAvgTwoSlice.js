00b29f2446f34aa070b403091c56cec8177fabb4
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-23 12:32:05 +0000

    minAvgTwoSlice wip

--- codeWarsSolutions/minAvgTwoSlice.js
+++ codeWarsSolutions/minAvgTwoSlice.js
@@ -1,30 +1,28 @@
-// module.exports = function minAvgTwoSlice(input) {
-//   const l = input.length;
-//   let sums = [];
-//   let avg;
+module.exports = function minAvgTwoSlice(inputNumbers) {
+  let sums = [];
 
-//   for (let i = 0; i < l; i++) {
-//     if (i === l - 1) {
-//       avg = input.reduce((total, number) => (total = total + number)) / l;
-//       sums.push(avg);
-//     } else {
-//       avg = (input[i] + input[i + 1]) / 2;
-//       sums.push(avg);
-//     }
-//   }
+  for (let i = 0; i < inputNumbers.length; i++) {
+    if (i === inputNumbers.length - 1) {
+      sums.push(
+        inputNumbers.reduce((total, number) => (total = total + number)) /
+          inputNumbers.length
+      );
+    } else {
+      sums.push((inputNumbers[i] + inputNumbers[i + 1]) / 2);
+    }
+  }
 
-//   const lowestAvg = Math.min(...sums);
-//   const lowestIndex = sums.indexOf(lowestAvg);
-//   //if 0 starts at 0
-//   //if 1 starts at 0 + 1
-//   //if 2 starts at prev (0 +1) + 1
+  console.log(sums);
+  const lowestAvg = Math.min(...sums);
+  const lowestIndex = sums.indexOf(lowestAvg);
+  return lowestIndex;
+};
 
-//   }
-//   console.log(lowestIndex);
-//   return sums;
-// };
+// 4 2 2 5 = 3.25
 
-// //[2, 1, 4]
-// // 2 1 =  avg 1.5
-// // 1 4 = avg 2.5
-// //2 1 4 = sum 7 avg 2.333
+// 4 2 2 = 2.66
+// 2 2 5  = 3
+
+// 4 2 = 3
+// 2 2 = 2
+// 2 5 = 3.5
--- test/minAvgTwoSlice.spec.js
+++ test/minAvgTwoSlice.spec.js
@@ -1,16 +1,17 @@
 const expect = require("chai").expect;
 const mindAvgTwoSlice = require("../codeWarsSolutions/minAvgTwoSlice");
 
-describe("Return starting position of the slice with the minimal avergae", function() {
-  it("", function() {
-    expect(mindAvgTwoSlice([2, 1, 4])).equal(2);
-  });
+describe.only("Return starting position of the slice with the smallest average", function() {
+  [{ it: [2, 1, 4], expect: 0 }, { it: [4, 2, 2, 5], expect: 1 }].forEach(run =>
+    it(`Given input of ${run.it} the lowest smallest average starts at ${run.expect}`, function() {
+      expect(mindAvgTwoSlice(run.it)).to.equal(run.expect);
+    })
+  );
 });
 
-// 2 1 =  avg 1.5
-// 1 4 = avg 2.5
-//2 1 4 = avg 2.33
-
-// returns the starting position of the slice with the minimal average.
-// If there is more than one slice with a minimal average,
-//  you should return the smallest starting position of such a slice
+// 4 2 = 3
+// 2 2 = 2
+// 2 5 = 3.5
+// 4 2 2 = 2.66
+// 2 2 5  = 3
+// 4 2 2 5 = 3.25
