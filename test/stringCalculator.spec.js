1b6ea671f53d3f845544c88e385236a917ffad6b
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-26 22:07:47 +0000

    rename

--- test/stringCalculator.spec.js
+++ test/stringCalculator.spec.js
@@ -1,13 +1,13 @@
 const calculator = require("../codeWarsSolutions/stringCalculator");
 const expect = require("chai").expect;
 
-describe.only("Given general number convert to roman numerals", function() {
+describe.only("String Calculator", function() {
   [
     { it: "1.1,2.2", expect: "3.3" },
     { it: "", expect: "0" },
     { it: "1.0,2.0", expect: "3.0" }
   ].forEach(run =>
-    it(`should convert ${run.it} to a  roman numeral ${run.expect}`, () => {
+    it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
       expect(calculator(run.it)).to.equal(run.expect);
     })
   );
