dda3ce06e7de01a21366c2d6b8aa9d5e4e42e280
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-29 20:56:35 +0100

    string calculator custom delimeter

--- codeWarsSolutions/stringCalculator.js
+++ codeWarsSolutions/stringCalculator.js
@@ -1,13 +1,35 @@
 module.exports = function stringCalculator(numbers) {
-  if (numbers.includes(",\n"))
+  if (numbers.match(/\/\/(.*?)\\n/g)) {
+    const delimeter = numbers
+      .match(/\/\/(.*?)\\n/g)[0]
+      .replace(/\/\//, "")
+      .replace(/\\n/, "");
+    const beginingOfInput = numbers.indexOf("\\n") + 2;
+    const answer = convertToNumberArray(
+      numbers.substring(beginingOfInput),
+      delimeter
+    ).reduce(sum);
+    return answer === 0.0 ? answer.toFixed() : answer.toFixed(1);
+  }
+  if (numbers.includes(",\n")) {
     return `Number expected but '\\n' found at position ${numbers.indexOf(
       ",\n"
     ) + 1}.`;
+  }
   if (numbers.charAt(numbers.length - 1) === ",")
     return "Number expected but EOF found";
-  const numberArray = numbers
-    .split(/\n|,/)
-    .map(numberAsString => Number(numberAsString));
-  let answer = numberArray.reduce((total, number) => (total += number));
+  const numberArray = convertToNumberArray(numbers, /\n|,/);
+  let answer = numberArray.reduce(sum);
   return answer === 0.0 ? answer.toFixed() : answer.toFixed(1);
 };
+
+function convertToNumberArray(numbers, delimeter) {
+  return numbers.split(delimeter).map(numberAsString => Number(numberAsString));
+}
+
+function sum(total, number) {
+  return (total += number);
+}
+//   "//;\n1;2", expect: "3.0" },
+
+//my regex /\/\/(.*?)\\n/g
--- test/stringCalculator.spec.js
+++ test/stringCalculator.spec.js
@@ -26,11 +26,33 @@ describe.only("String Calculator", () => {
       })
     );
   });
-  describe.only("should not allow input to end in a seperator", () => {
-    [{ it: "1,3,", expect: "Number expected but EOF found" }].forEach(run =>
+  describe("should not allow input to end in a seperator", () => {
+    [
+      { it: "1,3,", expect: "Number expected but EOF found" },
+      { it: "1,4,/n3,", expect: "Number expected but EOF found" }
+    ].forEach(run =>
+      it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
+        expect(calculator(run.it)).to.equal(run.expect);
+      })
+    );
+  });
+  describe("should be able to handle specified delimeter", () => {
+    [
+      { it: "1//;\\n1;2", expect: "3.0" }
+      // { it: "//|\n1|2|3,", expect: "6.0" },
+      // { it: "//sep\n2sep3", expect: "5.0" },
+      // { it: "//|\n1|2,3", expect: "'|' expected but ',' found at position 3." }
+    ].forEach(run =>
       it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
         expect(calculator(run.it)).to.equal(run.expect);
       })
     );
   });
 });
+
+// //[delimiter]\n[numbers]
+// "//;\n1;2" should return "3"
+// "//|\n1|2|3" should return "6"
+// "//sep\n2sep3" should return "5"
+// "//|\n1|2,3" is invalid and should return the message "'|' expected but ',' found at position 3."
+// All existing scenarios should work as before.
