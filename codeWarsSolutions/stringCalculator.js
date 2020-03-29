2e5eda88620d6b86ec986b9f1434009756a5538b
Saule Guzyte <saule.guzyte@tesco.com>
2020-03-29 16:43:26 +0100

    StringCalculator up to custom errors

--- codeWarsSolutions/stringCalculator.js
+++ codeWarsSolutions/stringCalculator.js
@@ -1,6 +1,12 @@
 module.exports = function stringCalculator(numbers) {
+  if (numbers.includes(",\n"))
+    return `Number expected but '\\n' found at position ${numbers.indexOf(
+      ",\n"
+    ) + 1}.`;
+  if (numbers.charAt(numbers.length - 1) === ",")
+    return "Number expected but EOF found";
   const numberArray = numbers
-    .split(",")
+    .split(/\n|,/)
     .map(numberAsString => Number(numberAsString));
   let answer = numberArray.reduce((total, number) => (total += number));
   return answer === 0.0 ? answer.toFixed() : answer.toFixed(1);
--- test/stringCalculator.spec.js
+++ test/stringCalculator.spec.js
@@ -1,14 +1,36 @@
 const calculator = require("../codeWarsSolutions/stringCalculator");
 const expect = require("chai").expect;
 
-describe.only("String Calculator", function() {
-  [
-    { it: "1.1,2.2", expect: "3.3" },
-    { it: "", expect: "0" },
-    { it: "1.0,2.0", expect: "3.0" }
-  ].forEach(run =>
-    it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
-      expect(calculator(run.it)).to.equal(run.expect);
-    })
-  );
+describe.only("String Calculator", () => {
+  describe("should add unknown number of integers", () => {
+    [
+      { it: "1.1,2.2", expect: "3.3" },
+      { it: "", expect: "0" },
+      { it: "1.0,2.0", expect: "3.0" }
+    ].forEach(run =>
+      it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
+        expect(calculator(run.it)).to.equal(run.expect);
+      })
+    );
+  });
+  describe("should interpret newline character as seperator", () => {
+    [
+      { it: "1\n2,3", expect: "6.0" },
+      {
+        it: "175.2,\n35",
+        expect: "Number expected but '\\n' found at position 6."
+      }
+    ].forEach(run =>
+      it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
+        expect(calculator(run.it)).to.equal(run.expect);
+      })
+    );
+  });
+  describe.only("should not allow input to end in a seperator", () => {
+    [{ it: "1,3,", expect: "Number expected but EOF found" }].forEach(run =>
+      it(`should sum up ${run.it} and return the sum of ${run.expect}`, () => {
+        expect(calculator(run.it)).to.equal(run.expect);
+      })
+    );
+  });
 });
